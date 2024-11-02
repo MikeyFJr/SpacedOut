8660ef6de1a31e251581e2efbf914559eff6bda4
appledcrab <124226900+appledcrab@users.noreply.github.com>
2024-11-02 16:29:58 -0400

    Adds stellas sprite sheet and adds her animations into game

--- so-godot-project/Scripts/Player/stella_script.gd
+++ so-godot-project/Scripts/Player/stella_script.gd
@@ -1,21 +1,22 @@
 extends CharacterBody2D
+class_name PlayerController
 
 #copied from previous 
 
 const DEBUG = true
 
-#const SPEED = 300.0 # Base horizontal movement speed
-#const ACCELERATION = 1000.0 # Base acceleration
-#const FRICTION = 1700.0 # Base friction
-#const GRAVITY = 2000.0 # Gravity when moving upwards
-#const FALL_GRAVITY = 3000.0 # Gravity when falling downwards
-#const FAST_FALL_GRAVITY = 5000.0 # Gravity while holding "fast_fall"
-#const WALL_GRAVITY = 25.0 # Gravity while sliding on a wall
-#const JUMP_VELOCITY = -500.0 # Maximum jump strength
-#const WALL_JUMP_VELOCITY = -700.0 # Maximum wall jump strength
-#const WALL_JUMP_PUSHBACK = 300.0 # Horizontal push strength off walls
-#const INPUT_BUFFER_PATIENCE = 0.1 # Input queue patience time
-#const COYOTE_TIME = 0.08 # Coyote patience time
+#const SPEED = 300.0 
+#const ACCELERATION = 1000.0 
+#const FRICTION = 1700.0 
+#const GRAVITY = 2000.0 
+#const FALL_GRAVITY = 3000.0 
+#const FAST_FALL_GRAVITY = 5000.0 
+#const WALL_GRAVITY = 25.0
+#const JUMP_VELOCITY = -500.0
+#const WALL_JUMP_VELOCITY = -700.0 
+#const WALL_JUMP_PUSHBACK = 300.0 
+#const INPUT_BUFFER_PATIENCE = 0.1 
+#const COYOTE_TIME = 0.08 
 
 const SPEED = 200.0 # Base horizontal movement speed
 const ACCELERATION = 700.0 # Base acceleration
@@ -32,11 +33,13 @@ const COYOTE_TIME = 0.08 # Coyote patience time
 
 const BOOST_VELOCITY = -800.0 # strength for boost
 
-
+var direction = 0
 
 var input_buffer : Timer # Reference to the input queue timer
 var coyote_timer : Timer # Reference to the coyote timer
 var coyote_jump_available := true
+var horizontal_input
+
 
 
 
@@ -59,7 +62,7 @@ func _ready() -> void:
 func _physics_process(delta) -> void:
 	#anim_player.play("idle")
 	# Get inputs
-	var horizontal_input := Input.get_axis("move_left", "move_right")
+	horizontal_input = Input.get_axis("move_left", "move_right")
 	var jump_attempted := Input.is_action_just_pressed("jump")
 	var boost_attempted :=Input.is_action_just_pressed("boost")
 
@@ -97,15 +100,6 @@ func _physics_process(delta) -> void:
 
 	# HYandle horizontal motion and friction
 	var floor_damping := 1.0 if is_on_floor() else 0.5 # Set floor damping, friction is less when in air
-	#var dash_multiplier
-	#if Input.is_action_pressed("boost") and boosts_available > 0:
-		#print("boosted")
-		#dash_multiplier =4
-		#boosts_available -=1
-	#else:
-		#dash_multiplier =1
-		
-	#var dash_multiplier := 2 if Input.is_action_pressed("boost") and boosts_available > 0 else 1
 
 	if horizontal_input and jump_attempted and not boost_attempted:
 		velocity.x = (horizontal_input * SPEED/4) 
@@ -113,7 +107,9 @@ func _physics_process(delta) -> void:
 		velocity.x = horizontal_input * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, (FRICTION * delta) * floor_damping)
-		
+	
+#	cant tell which way I want to do it
+
 	#if horizontal_input:
 		#velocity.x = move_toward(velocity.x, horizontal_input * SPEED, ACCELERATION * delta)
 	#else:
