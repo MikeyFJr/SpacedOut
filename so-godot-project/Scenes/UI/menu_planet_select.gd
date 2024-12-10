ca0c895651d8ed7d89fab0a0c6c03e142f0a311a
Brooke :) <133280277+nicolesgetaway@users.noreply.github.com>
2024-12-10 17:58:39 -0500

    created planet select script

---
+++ so-godot-project/Scenes/UI/menu_planet_select.gd
@@ -0,0 +1,20 @@
+extends Control
+
+@export var world_1_play : Button
+@export var world_2_play : Button
+@export var world_3_play : Button
+@export var world_4_play : Button
+@export var next_btn : Button
+
+@export var world_label : Label
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
